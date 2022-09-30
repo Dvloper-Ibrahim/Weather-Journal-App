f16f23b9b2ca0196c51247b9a9de479e59846d91
Ibrahim Ahmed <ibrahimahmed90000@gmail.com>
2022-10-01 01:57:56 +0200

    Updates

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 app.use(cors());
 // Initialize the main project folder
-app.use(express.static('./'));
+app.use(express.static('website'));
 // Setup Server
 const port = 8000;
 app.listen(port, () => {
